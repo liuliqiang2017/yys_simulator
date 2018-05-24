0ffde0f8ace629b1c6b7c3af2a87e249bf4a6f65
liuliqiang <wrthew@163.com>
2018-05-24 15:19:08 +0800

    添加了一批buff/debuff 第一批人物的都有了

--- lib/buff.py
+++ lib/buff.py
@@ -1,32 +1,136 @@
 """
 buff模块，包括各种增益减益等常时buff（例如星灭等）控制buff（由于伤害模拟用不到先不做）。 其他类似于土蜘蛛这种伤害buff待考虑。
 生成buff实例所需要的参数：释放者caster，目标target
-自有属性layer：层数， update_period：层数更新时间，有3个值：1-回合前，2-攻击时， 3-回合后
-buff提供如下方法：add:将生成的buff实例添加到target的buff_list里， remove:删除target的buff_list里的self，
+自有属性: id:buff的标识，用来判断是否有重复buff(从100开始），layer：层数;coexist_num: 可以同时存在的相同id的buff的个数
+buff提供如下方法：add:将生成的buff实例添加到target的buff_list里， remove:删除target的buff_list里的self，layer_update: 在回合后用来更新层数
 """
 
-class StatusBuff:
+class baseBuff:
     "属性buff基类"
-    def __init__(self, caster, target):
+    def __init__(self, caster, target, val):
         super().__init__()
         self.caster = caster
         self.target = target
-        self.layer = None
-        self.update_period = None
+        self.val = val
+        self.config()
+        
+    def config(self):
+        self.coexist_num = 1
+        self.id = None
+        self.layer = 1
 
     def add(self):
-        self.target.buffs.append(self)
-        self._valid()
-    
+        same_buffs = [buff for buff in self.target.buffs if buff.id == self.id]
+        if len(same_buffs) < self.coexist_num:
+            self.target.buffs.append(self)
+            self._valid()
+        else:
+            same_buffs[0].remove()
+            self.add()
+
     def _valid(self):
         raise NotImplementedError
     
     def remove(self):
-        self.target.buffs.remove(self)
         self._invalid()
+        self.target.buffs.remove(self)        
     
     def _invalid(self):
         raise NotImplementedError
 
     def layer_update(self):
-        raise NotImplementedError
+        self.layer -= 1
+        if self.layer <= 0:
+            self.remove()
+
+class StatusBuff(baseBuff):
+    "增益buff"
+    pass
+
+class StatusDebuff(baseBuff):
+    "减益buff"
+    def add(self):
+        if not self.target.immune:
+            super().add()
+
+class Xing(StatusBuff):
+    "晴明星，ID 100"
+    def config(self):
+        self.coexist_num = 1
+        self.id = 100
+        self.layer = 3
+    
+    def _valid(self):
+        self.target.damage_ratio += 0.3
+    
+    def _invalid(self):
+        self.target.damage_ratio -= 0.3
+
+class Mie(StatusDebuff):
+    "晴明灭, ID 101"
+    def config(self):
+        self.coexist_num = 1
+        self.id = 101
+        self.layer = 3
+    
+    def _valid(self):
+        self.target.harm_ratio += 0.3
+    
+    def _invalid(self):
+        self.target.harm_ratio -= 0.3
+
+class SteelFeather(StatusBuff):
+    "天狗的钢铁毛"
+    def config(self):
+        self.coexist_num = 1
+        self.id = 102
+        self.layer = 1
+    
+    def _valid(self):
+        self.target.atk_ratio += 0.15
+        self.target.criDM += 15
+    
+    def _invalid(self):
+        self.target.atk_ratio -= 0.15
+        self.target.criDM -= 15
+
+class CurseFire(StatusDebuff):
+    "丑女咒火"
+    def config(self):
+        self.coexist_num = 1
+        self.id = 103
+        self.layer = 2
+    
+    def _valid(self):
+        self.target.harm_ratio += 0.15
+    
+    def _invalid(self):
+        self.target.harm_ratio -= 0.15
+
+class FuriousEyes(StatusDebuff):
+    "两面佛怒目"
+    def config(self):
+        self.coexist_num = 1
+        self.id = 104
+        self.layer = 2
+    
+    def _valid(self):
+        self.target.atk_ratio -= 0.16
+        self.target.def_ratio -= 0.16
+    
+    def _invalid(self):
+        self.target.atk_ratio += 0.16
+        self.target.def_ratio += 0.16
+
+class Fear(StatusBuff):
+    "陆生的畏"
+    def config(self):
+        self.coexist_num = 4
+        self.id = 105
+        self.layer = 4
+    
+    def _valid(self):
+        self.target.damage_ratio += 0.75
+    
+    def _invalid(self):
+        self.target.damage_ratio -= 0.75
